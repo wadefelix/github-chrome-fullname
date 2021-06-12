28915b138cd827953e302d6ce36d49c9262c18a9
Ren Wei <wadefelix@gmail.com>
2021-06-12 09:24:27 +0800

    relaceElement
    # Conflicts:
    #	src/api.ts

--- src/replacer.ts
+++ src/replacer.ts
@@ -19,7 +19,7 @@ export class NodeReplacer {
 
 
     public async watch(element: Element) {
-        await this.replace(element)
+        await this.replaceEle(element)
         const observer = new MutationObserver(this._getChangeHandler())
         observer.observe(element, {
             childList: true,
@@ -32,30 +32,36 @@ export class NodeReplacer {
     public _getChangeHandler() {
         return (mutations: MutationRecord[], _observer: MutationObserver) => {
             for(const { target } of mutations) {
-                this.replace(target)
+                this.replaceEle(target)
             }
         }
     }
 
     public async replaceEle(element: Node) {
         var commitauthor_eles = document.getElementsByClassName('commit-author')
+        console.dir(commitauthor_eles)
         var eles = document.getElementsByClassName('author')
         var author_eles  = Array.prototype.filter.call(eles, function(ele: Element) {
             return ele.hasAttribute('data-hovercard-type') && ele.getAttribute('data-hovercard-type') == "user";
         })
+        console.dir(author_eles)
         eles = document.getElementsByClassName('Link--muted')
         var linkmuted_eles = Array.prototype.filter.call(eles, function(ele: Element) {
             return ele.hasAttribute('data-hovercard-type') && ele.getAttribute('data-hovercard-type') == "user";
         })
+        console.dir(linkmuted_eles)
         const pending = []
         for (var i = 0; i < commitauthor_eles.length; i++) {
-            pending.push(this._replaceEle(commitauthor_eles[i]))
+            const curNode = commitauthor_eles[i]
+            pending.push(this._replaceEle(curNode))
         }
         for (var i = 0; i < author_eles.length; i++) {
-            pending.push(this._replaceEle(author_eles[i]))
+            const curNode = author_eles[i]
+            pending.push(this._replaceEle(curNode))
         }
         for (var i = 0; i < linkmuted_eles.length; i++) {
-            pending.push(this._replaceEle(linkmuted_eles[i]))
+            const curNode = linkmuted_eles[i]
+            pending.push(this._replaceEle(curNode))
         }
         await Promise.all(pending)
     }
